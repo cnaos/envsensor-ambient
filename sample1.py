85f1951574a664543677b8c32829d298e36bf79e
cnaos <cnaos.gm@gmail.com>
2019-01-23 20:17:10 +0900

    温度と湿度の出力のみに絞る

--- sample1.py
+++ sample1.py
@@ -23,7 +23,8 @@ while True:
     if data is not None:
 
         if data.tick_last_update > latest_update:
-            print('vars={}'.format(vars(data)))
+            print('temp: {}'.format(data.val_temp))
+            print('humi: {}'.format(data.val_humi))
             print('Illumination: {} lx'.format(data.val_light))
 
         latest_update = data.tick_last_update
